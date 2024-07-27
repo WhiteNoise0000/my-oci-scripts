e0f97dcfee4959fdd7b35533354302524eea83f5
とみ <46626065+WhiteNoise0000@users.noreply.github.com>
2024-07-27 14:10:08 +0900

    Add files via upload
    Object Strageへのバックアップスクリプト

---
+++ bk2oci-os/bk2oci-os.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# object storage upload
+cd /mnt/hdd/share
+cat bkup_target.txt | xargs -i oci os object sync -bn t-bkup --src-dir {} --no-multipart --delete --prefix {}/ --exclude .sync --exclude Thumbs.db
